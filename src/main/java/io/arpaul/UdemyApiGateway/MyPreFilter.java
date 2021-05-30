3901045c916c5d21a53df2696988244d0fbfd882
Aritra1704 <aritra1704@gmail.com>
2021-05-30 19:49:53 +0530

    Added Authorization Header filter and one pre filter for JWT token authorization.

---
+++ src/main/java/io/arpaul/UdemyApiGateway/MyPreFilter.java
@@ -0,0 +1,34 @@
+package io.arpaul.UdemyApiGateway;
+
+import java.util.Set;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.cloud.gateway.filter.GatewayFilterChain;
+import org.springframework.cloud.gateway.filter.GlobalFilter;
+import org.springframework.http.HttpHeaders;
+import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+
+import reactor.core.publisher.Mono;
+
+@Component
+public class MyPreFilter implements GlobalFilter {
+
+	final Logger logger = LoggerFactory.getLogger(MyPreFilter.class);
+	
+	@Override
+	public Mono<Void> filter(ServerWebExchange exchange, GatewayFilterChain chain) {
+		logger.info("My first pre filter is executed...");
+		String requestPath = exchange.getRequest().getPath().toString();
+		logger.info("Request Path: "+requestPath);
+		HttpHeaders httpheader = exchange.getRequest().getHeaders();
+		Set<String> headerNames = httpheader.keySet();
+		headerNames.forEach((headerName) -> {
+			String headerValue = httpheader.getFirst(headerName);
+			logger.info(headerName+" "+headerValue);
+		});
+		return chain.filter(exchange);
+	}
+
+}
